11aaa028c0b66e7e6e3d731f3590868c696961e3
Arvind Kharwal <karvind629@gmail.com>
2023-03-14 11:37:18 +0530

    14 March 2023

---
+++ Unit 3/Inheritance/34.overriding.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    void f1()
+    {
+        cout << "f1 from class A" << endl;
+    }
+    void f2()
+    {
+        cout << "f2 from class A" << endl;
+    }
+};
+class B : public A
+{
+public:
+    void f1()
+    {
+        cout << "f1 from class B" << endl;
+    }
+    void f2(int x)
+    {
+        cout << "f2 from class B" << endl;
+        cout << "\n"
+             << x << endl;
+    }
+};
+int main()
+{
+    B B1;
+    B1.f1();
+    B1.f2(4);
+    return (0);
+}
---
+++ Unit 3/Inheritance/35.overriding.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    void f1()
+    {
+        cout << "f1 from class A" << endl;
+    }
+    void f2()
+    {
+        cout << "f2 from class A" << endl;
+    }
+};
+class B : public A
+{
+public:
+    void f1()
+    {
+        cout << "f1 from class B" << endl;
+    }
+    void f2(int x)
+    {
+        cout << "f2 from class B" << endl;
+        cout << "\n"<< x << endl;
+    }
+};
+int main()
+{
+    B B1;
+    B1.f1();
+    // B1.f2();    // Error, no matching function for call to 'B::f2()'
+    return (0);
+}
