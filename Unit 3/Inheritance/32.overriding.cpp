09a5cacd0bce13186d530b7a498545bf7bf9f575
Arvind Kharwal <karvind629@gmail.com>
2023-03-14 11:35:02 +0530

    14 March 2023

---
+++ Unit 3/Inheritance/32.overriding.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    void f1()
+    {
+        cout << "f1 from class A" << endl;
+    }
+    void f2()
+    {
+        cout << "f2 from class A" << endl;
+    }
+};
+class B : public A
+{
+public:
+    void f1()
+    {
+        cout << "f1 from class B" << endl;
+    }
+    void f2()
+    {
+        cout << "f2 from class B" << endl;
+    }
+};
+int main()
+{
+    B B1;
+    B1.f1();
+    B1.f2();
+    return (0);
+}
