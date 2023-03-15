438fd963918711c1db2ad94715659b87ab4f2897
Arvind Kharwal <karvind629@gmail.com>
2023-03-15 13:07:47 +0530

    15 March 2023

---
+++ Unit 3/Inheritance/38.inheritance-constructor.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class Example
+{
+    int a,b;
+    public:
+        Example()
+        {
+            cout<<"Default Constructor from Example Class"<<endl;
+        }
+        Example(int x,int y)
+        {
+            cout<<"Constructor from Example Class"<<endl;
+            a = x;
+            b = y;
+        }
+};
+class Test:public Example
+{
+    int c,d;
+    public:
+        Test(int x,int y)
+        {
+            cout<<"Constructor from Test Class"<<endl;
+            c = x;
+            d = y;
+        }
+};
+int main()
+{
+    Test T1(10,20);
+    return(0);
+}
