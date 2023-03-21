76e28e768fbabec8c69ed8f19fb06bd127c9bbd1
Arvind Kharwal <karvind629@gmail.com>
2023-03-21 13:20:01 +0530

    21 March 2023

---
+++ Unit 3/Virtual Function/6.virtual.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+class Animal
+{
+private:
+    string type;
+
+public:
+    Animal()
+    {
+        type = "Animal";
+    }
+    virtual void display()
+    {
+        cout << type << endl;
+    }
+};
+
+class Dog : public Animal
+{
+private:
+    string type;
+
+public:
+    Dog()
+    {
+        type = "Dog";
+    }
+    void display()
+    {
+        cout << type << endl;
+    }
+};
+
+class Cat : public Animal
+{
+private:
+    string type;
+
+public:
+    Cat()
+    {
+        type = "Cat";
+    }
+    void display()
+    {
+        cout << type << endl;
+    }
+};
+int main()
+{
+    Animal A1, *P;
+    Dog D1;
+    Cat C1;
+    P = &A1;
+    P->display();
+    P = &D1;
+    P->display();
+    P = &C1;
+    P->display();
+    return (0);
+}
