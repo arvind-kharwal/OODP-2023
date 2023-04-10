aa3ef0e7c6d607ccfedff63367b2b70177fad667
Arvind Kharwal <karvind629@gmail.com>
2023-04-10 12:03:59 +0530

    10 April 2023

---
+++ Unit 4/Templates/12.template-class.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+template <class T>
+class Example
+{
+    T a, b;
+
+public:
+    void getdata(T x, T y)
+    {
+        a = x;
+        b = y;
+    }
+    void display()
+    {
+        cout << a << " " << b << endl;
+    }
+};
+int main()
+{
+    Example<int> E1;
+    E1.getdata(10, 20);
+    E1.display();
+    cout << "------" << endl;
+    Example<double> E2;
+    E2.getdata(12.3, 56.4);
+    E2.display();
+    cout << "------" << endl;
+    Example<string> E3;
+    E3.getdata("Hello", "Arvind");
+    E3.display();
+    return (0);
+}
