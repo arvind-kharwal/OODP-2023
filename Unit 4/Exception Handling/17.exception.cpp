dd9f1ba3684303e8670488095897e7f5e7ae106c
Arvind Kharwal <karvind629@gmail.com>
2023-04-12 12:04:27 +0530

    12 April 2023

--- Unit 4/Exception Handling/17.exception.cpp
+++ Unit 4/Exception Handling/17.exception.cpp
@@ -1,38 +1,20 @@
-// We need to mention it in the divide function
 #include <iostream>
 using namespace std;
-class Example
-{
-    int a, b;
-
-public:
-    void getdata(int x, int y)
-    {
-        a = x;
-        b = y;
-    }
-    void divide()
-    {
-        try
-        {
-            if (b == 0)
-                throw(5);
-            cout << "The division is: " << a / b << endl;
-        }
-        catch (int e)
-        {
-            cout << "Division is not possible" << endl;
-        }
-    }
-};
 int main()
 {
-    Example E1;
-    E1.getdata(20, 4);
-    E1.divide();
-    cout << "----------" << endl;
-    Example E2;
-    E2.getdata(4, 0);
-    E2.divide();
+    int a, b;
+    cout << "Enter the numbers: " << endl;
+    cin >> a >> b;
+    try
+    {
+        if (b == 0)
+            throw(10);
+        else
+            cout << "The division is: " << a / b;
+    }
+    catch (int e)
+    {
+        cout << "Division is not possible" << endl;
+    }
     return (0);
 }
