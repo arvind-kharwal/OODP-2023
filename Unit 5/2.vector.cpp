235566eed960c10f40a9d69b999db238b70ee8fa
Arvind Kharwal <karvind629@gmail.com>
2023-04-13 15:19:24 +0530

    13 April 2023

---
+++ Unit 5/2.vector.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> V = {1,2,3,4,5,6,7,8,9,10};
+    vector<int>::iterator i;
+    i = V.begin();
+    advance(i,5);
+    cout<<*i<<endl;
+    advance(i,-2);
+    cout<<*i<<endl;
+
+}
