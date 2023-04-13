efcd3b7b59e49c3f94202ac302f7f0c616f6aec3
Arvind Kharwal <karvind629@gmail.com>
2023-04-13 15:52:39 +0530

    13 April 2023

--- Unit 5/7.vector.cpp
+++ Unit 5/7.vector.cpp
@@ -5,8 +5,8 @@ int main()
 {
     vector<int> V = {1,2,3,4,5,6,7,8,9,10};
     vector<int>::iterator i,j;
-    i = V.begin();
-    j = next(i); // std::next() will advance by one by default, whereas std::advance() requires a distance.
-    cout<<*i<<" "<<*j<<endl;
+    i = V.end();
+    j = prev(i);
+    cout<<*j<<endl;
     return(0);
 }
