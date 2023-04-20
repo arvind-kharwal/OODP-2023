19ddc5640d6cc937197b1161395c921614001200
Arvind Kharwal <karvind629@gmail.com>
2023-04-20 12:11:49 +0530

    20 April 2023

---
+++ Unit 5/Vector/9.vector.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    for (int i = 0; i < 5; i++)
+        v.push_back(i);
+    for (auto i = v.begin(); i != v.end(); i++)
+    {
+        cout << *i << " ";
+    }
+    return (0);
+}
