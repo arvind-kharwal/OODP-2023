5eb51f3f56f37568497cceb5d636240ebe883433
Arvind Kharwal <karvind629@gmail.com>
2023-04-20 13:33:23 +0530

    20 April 2023

---
+++ Unit 5/Vector/22.vector.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+int main()
+{
+    vector<int> v = {1, 2, 3, 4};
+    vector<int>::iterator i;
+    for (i = v.begin(); i != v.end(); i++)
+    {
+        cout << *i << " ";
+    }
+    v.erase(v.begin());
+    cout<<"\nAfter erasing first element"<<endl;
+    for (i = v.begin(); i != v.end(); i++)
+    {
+        cout << *i << " ";
+    }
+return(0);
+}
