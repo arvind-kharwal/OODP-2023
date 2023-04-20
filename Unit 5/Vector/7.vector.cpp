3e7e84abfcc97d046e3f4b169615bcef7e018aa8
Arvind Kharwal <karvind629@gmail.com>
2023-04-20 12:10:38 +0530

    20 April 2023

---
+++ Unit 5/Vector/7.vector.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    for (int i = 0; i < 5; i++)
+        v.push_back(i);
+    for (int i : v)
+        cout << i << " ";
+    return (0);
+}
