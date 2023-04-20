bbe5b4c1b98c9e4ce5734619dd9882f86714d977
Arvind Kharwal <karvind629@gmail.com>
2023-04-20 12:09:38 +0530

    20 April 2023

--- Unit 5/Vector/1.vector.cpp
+++ Unit 5/Vector/1.vector.cpp
@@ -4,34 +4,39 @@ using namespace std;
 int main()
 {
     vector<int> v;
-    cout<<v.capacity()<<endl;
-    cout<<v.size()<<endl;
+    cout << v.capacity() << endl;
+    cout << v.size() << endl;
     v.push_back(1);
-    cout<<v.capacity()<<endl;
-    cout<<v.size()<<endl;
+    cout << v.capacity() << endl;
+    cout << v.size() << endl;
     v.push_back(2);
-    cout<<v.capacity()<<endl;
-    cout<<v.size()<<endl;
+    cout << v.capacity() << endl;
+    cout << v.size() << endl;
     v.push_back(3);
-    cout<<v.capacity()<<endl;
-    cout<<v.size()<<endl;
+    cout << v.capacity() << endl;
+    cout << v.size() << endl;
     v.push_back(4);
-    cout<<v.capacity()<<endl;
-    cout<<v.size()<<endl;
+    cout << v.capacity() << endl;
+    cout << v.size() << endl;
     v.push_back(5);
-    cout<<v.capacity()<<endl;
-    cout<<v.size()<<endl;
+    cout << v.capacity() << endl;
+    cout << v.size() << endl;
     v.push_back(6);
-    cout<<v.capacity()<<endl;
-    cout<<v.size()<<endl;
+    cout << v.capacity() << endl;
+    cout << v.size() << endl;
     v.push_back(7);
-    cout<<v.capacity()<<endl;
-    cout<<v.size()<<endl;
+    cout << v.capacity() << endl;
+    cout << v.size() << endl;
     v.push_back(8);
-    cout<<v.capacity()<<endl;
-    cout<<v.size()<<endl;
+    cout << v.capacity() << endl;
+    cout << v.size() << endl;
     v.push_back(9);
-    cout<<v.capacity()<<endl;
-    cout<<v.size()<<endl;
-    return(0);
+    cout << v.capacity() << endl;
+    cout << v.size() << endl;
+    vector<int>::iterator i;
+    for (i = v.begin(); i != v.end(); i++)
+    {
+        cout << *i << " ";
+    }
+    return (0);
 }
