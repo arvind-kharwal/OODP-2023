28226e3058d80b35b5fb30851028384d284db51d
Arvind Kharwal <karvind629@gmail.com>
2023-04-20 12:16:49 +0530

    20 April 2023

---
+++ Unit 5/Vector/15.vector.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+int main()
+{
+    vector<int> v = {1, 2, 3, 4, 5, 6};
+    vector<int>::iterator i;
+    for (i = v.begin(); i != v.end(); i++)
+    {
+        cout << *i << " ";
+    }
+    v.insert(v.begin() + 2, 100);
+    cout << "\nafter inserting " << endl;
+    for (i = v.begin(); i != v.end(); i++)
+    {
+        cout << *i << " ";
+    }
+    return (0);
+}
