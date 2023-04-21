576d903caf0a1c8aac499aac119a945cfda58524
Arvind Kharwal <karvind629@gmail.com>
2023-04-21 13:42:42 +0530

    21 April 2023

--- Unit 5/Iterator/6.iterator.cpp
+++ Unit 5/Iterator/6.iterator.cpp
@@ -6,7 +6,9 @@ int main()
     vector<int> V = {1,2,3,4,5,6,7,8,9,10};
     vector<int>::iterator i,j;
     i = V.begin();
+    i++;
     j = V.end();
+    j--;
     cout<<distance(i,j)<<endl;
     return(0);    
 }
