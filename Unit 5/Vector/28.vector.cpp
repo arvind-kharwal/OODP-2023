12f2ef6f638775c1fc4ec1d9ed498c73a303bdef
Arvind Kharwal <karvind629@gmail.com>
2023-04-24 16:06:07 +0530

    24 April 2023

--- Unit 5/Vector/28.vector.cpp
+++ Unit 5/Vector/28.vector.cpp
@@ -5,8 +5,10 @@ int main()
 {
     vector<int> v={1,2,3};
     v.assign(6,2);
-    for(auto i = v.begin();i!=v.end();i++)
-    {
-        cout<<*i<<" ";
-    }
+    for(int i:v)
+        cout<<i<<" ";
+    // for(auto i = v.begin();i!=v.end();i++)
+    // {
+    //     cout<<*i<<" ";
+    // }
 }
