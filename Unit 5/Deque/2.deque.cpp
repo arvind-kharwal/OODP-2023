7be5d1e3da853360613f282a1125360cd45e4097
Arvind Kharwal <karvind629@gmail.com>
2023-04-25 09:57:35 +0530

    25 April 2023

---
+++ Unit 5/Deque/2.deque.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+int main()
+{
+    deque<int> D ={ 1,2,3,4,5};
+    deque<int>::iterator i,j;
+    for(i=D.begin();i!=D.end();i++)
+        cout<<*i<<" ";
+    return(0);
+}
