0ec791d66fb1f1ce7d407aec6f95d1e75ce41466
Arvind Kharwal <karvind629@gmail.com>
2023-04-25 16:07:53 +0530

    25 April 2023

---
+++ Unit 5/Stack/4.stack.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main()
+{
+    stack<int> S;
+    S.push(5);
+    S.push(10);
+    S.push(15);
+    cout<<S.size()<<" ";
+    cout<<S.empty()<<" ";
+    return(0);
+}
