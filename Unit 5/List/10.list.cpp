3664ee5aa7def97d3a017f907986e0346c1ac9c8
Arvind Kharwal <karvind629@gmail.com>
2023-04-25 09:12:38 +0530

    25 April 2023

---
+++ Unit 5/List/10.list.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <list>
+using namespace std;
+int main()
+{
+    list<int> L1 = {10, 2, 43, 14};
+    L1.sort(greater<int>());
+    for (auto i = L1.begin(); i != L1.end(); i++)
+        cout << *i << " ";
+    return (0);
+}
