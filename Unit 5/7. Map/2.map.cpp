20d33e33a363821c4847ca581d1fe6bdeb475153
Arvind Kharwal <karvind629@gmail.com>
2023-04-27 12:34:01 +0530

    27 April 2023

---
+++ Unit 5/7. Map/2.map.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <map>
+using namespace std;
+int main()
+{
+    map<int, string> M;
+    map<int, string>::iterator i;
+    M[1]="Arvind";
+    M[2]="Amit";
+    M[3]="C++";
+    for (i = M.begin(); i != M.end(); i++)
+        cout << (*i).first << ": " << (*i).second << endl;
+    return (0);
+}
