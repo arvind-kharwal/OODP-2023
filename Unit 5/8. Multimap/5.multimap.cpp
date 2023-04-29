31bae4c5092b5777fcd96464c72ecb5e1aad5e96
Arvind Kharwal <karvind629@gmail.com>
2023-04-29 09:58:47 +0530

    29 April 2023

---
+++ Unit 5/8. Multimap/5.multimap.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<map>
+using namespace std;
+int main()
+{
+    multimap<int,int> M;
+    M.insert(pair<int,int>(1,10));
+    M.insert(pair<int,int>(2,20));
+    M.insert(pair<int,int>(3,30));
+    M.insert(pair<int,int>(4,40));
+    M.insert(pair<int,int>(5,50));
+    M.insert(pair<int,int>(2,100));
+    multimap<int,int>::iterator i;
+    for(i=M.begin();i!=M.end();i++)
+        cout<<(*i).first<<":"<<(*i).second<<endl;
+    cout<<M.size()<<endl;
+    return(0);
+}
