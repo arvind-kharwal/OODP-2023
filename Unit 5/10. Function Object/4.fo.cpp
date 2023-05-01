a0455701fc748c958325dac1dc010391f308f183
Arvind Kharwal <karvind629@gmail.com>
2023-05-01 16:11:45 +0530

    1 May 2023

---
+++ Unit 5/10. Function Object/4.fo.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+#include<algorithm>
+#include<functional>
+int increment(int x)
+{
+    x = x+1;
+    return(x);
+}
+int main()
+{
+    int a[] = {1,2,3,4};
+    int res[4];
+    for(int i=0;i<3;i++)
+        cout<<a[i]<<" ";
+    transform(a,a+4,res,increment);
+    cout<<"\nAfter Increment"<<endl;
+    for(int i=0;i<3;i++)
+        cout<<res[i]<<" ";
+    return(0);
+}
