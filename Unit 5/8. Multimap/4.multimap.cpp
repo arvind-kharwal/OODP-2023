48d110f6dcce104ef99d0b3d9acdcf7d984da97f
Arvind Kharwal <karvind629@gmail.com>
2023-05-01 11:48:47 +0530

    1 May 2023

--- Unit 5/8. Multimap/4.multimap.cpp
+++ Unit 5/8. Multimap/4.multimap.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-    multimap<int,string> M =  {{1, "Arvind"}, {2, "Adesh"}, {3, "Aryadeep"}};
+    multimap<int,string> M =  {{1, "Arvind"}, {2, "Adesh"}, {2, "Aryadeep"}};
     multimap<int,string>::iterator i;
     for(i=M.begin();i!=M.end();i++)
         cout<<(*i).first<<":"<<(*i).second<<endl;
