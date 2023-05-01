d4854a4793e64cb1ccc898eb471288d7c7679cc7
Arvind Kharwal <karvind629@gmail.com>
2023-05-01 12:02:12 +0530

    1 May 2023

---
+++ Unit 5/9. Functions/2.count.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> v = {1,2,3,4,3,6,3,8,9};
+    int n;
+    n = count(v.begin(),v.end(),3);
+    cout<<"The count is: "<<n<<endl;
+    return(0);
+}
