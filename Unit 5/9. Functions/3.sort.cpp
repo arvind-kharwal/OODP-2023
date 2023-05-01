315737f4cda7d1339f11073fe59a9593a77ceb46
Arvind Kharwal <karvind629@gmail.com>
2023-05-01 12:06:24 +0530

    1 May 2023

--- Unit 5/9. Functions/3.sort.cpp
+++ Unit 5/9. Functions/3.sort.cpp
@@ -5,10 +5,7 @@ using namespace std;
 int main()
 {
     vector<int> v = {11,20,3,41,13,61,23,58,9};
-    vector<int>:: iterator i,j;
-    j = v.end();
-    j = j-2;
-    sort(v.begin(),j);
+    sort(v.begin(),v.end());
     for(int i:v)
         cout<<i<<" ";
     return(0);
