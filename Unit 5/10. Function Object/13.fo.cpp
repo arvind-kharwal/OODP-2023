152b39789bf0ea7e654d2ddab5ed257372930140
Arvind Kharwal <karvind629@gmail.com>
2023-05-02 09:36:30 +0530

    2 May 2023

---
+++ Unit 5/10. Function Object/13.fo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <functional>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int a[] = {1, 2, 3, 4, 5};
+    int b[] = {10, 20, 30, 40, 50};
+    int r[5];
+    transform(b, b + 5, a, r, multiplies<>());
+    for (int i = 0; i < 5; i++)
+        cout << r[i] << " ";
+    return (0);
+}
