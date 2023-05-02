b1af273a2e3655291e18fe89818fa177fd0bebad
Arvind Kharwal <karvind629@gmail.com>
2023-05-02 10:09:24 +0530

    2 May 2023

---
+++ Unit 5/11. Set/1.set.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<set>
+using namespace std;
+int main()
+{
+    set <int> s = {1,2,3,4};
+    set<int>::iterator i;
+    for(i=s.begin();i!=s.end();i++)
+        cout<<*i<<" ";
+    return(0);
+}
