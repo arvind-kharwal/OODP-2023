89ffad776e125549791168e7be1d87233dc21b22
Arvind Kharwal <karvind629@gmail.com>
2023-05-02 09:36:03 +0530

    2 May 2023

---
+++ Unit 5/10. Function Object/12.fo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#include <vector>
+int main()
+{
+    vector<int> v1 = {1, 2, 3, 4, 5};
+    vector<int> v2 = {10, 20, 30, 40, 50};
+    vector<int> v(5);
+    transform(v1.begin(), v1.end(), v2.begin(), v.begin(), plus<int>());
+    vector<int>::iterator i;
+    for (i = v.begin(); i != v.end(); i++)
+        cout << *i << " ";
+    return (0);
+}
