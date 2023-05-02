11b54f99d2261bd4dd1fe9e07be92b49f3a4f8b8
Arvind Kharwal <karvind629@gmail.com>
2023-05-02 10:04:28 +0530

    2 May 2023

---
+++ Unit 5/9. Functions/9.sort.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> v = {11,20,3,41,13,61,23,58,9};
+    sort(v.begin(),v.end(),greater<>());
+    for(int i:v)
+        cout<<i<<" ";
+    return(0);
+}
