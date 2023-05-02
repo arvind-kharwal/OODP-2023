c5784a5e163cbd6a04c8384e40f5cb4fd6a599c9
Arvind Kharwal <karvind629@gmail.com>
2023-05-02 09:29:21 +0530

    2 May 2023

--- Unit 5/10. Function Object/3.fo.cpp
+++ Unit 5/10. Function Object/3.fo.cpp
@@ -1,14 +1,14 @@
-#include<iostream>
+#include <iostream>
+#include <functional>
+#include <algorithm>
 using namespace std;
-#include<algorithm>
-#include<functional>
 int main()
 {
-    int a1[] = {1,2,3};
-    int a2[] = {4,5,6};
-    int res[3];
-    transform(a1,a1+3,a2,res,plus<int>());
-    for(int i=0;i<3;i++)
-        cout<<res[i];
-    return(0);
+    int a[] = {1, 2, 3, 4, 5};
+    int b[] = {10, 20, 30, 40, 50};
+    int r[5];
+    transform(a, a + 5, b, r, plus<>());
+    for (int i = 0; i < 5; i++)
+        cout << r[i] << " ";
+    return (0);
 }
