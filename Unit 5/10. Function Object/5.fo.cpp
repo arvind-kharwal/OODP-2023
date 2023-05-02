3ab8b4d3144947747ee3ec450b859e1e08006a02
Arvind Kharwal <karvind629@gmail.com>
2023-05-02 09:30:33 +0530

    2 May 2023

--- Unit 5/10. Function Object/5.fo.cpp
+++ Unit 5/10. Function Object/5.fo.cpp
@@ -1,21 +1,17 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-#include<algorithm>
-#include<functional>
 int increment(int x)
 {
-    x = x*2;
-    return(x);
+    x = x + 1;
+    return (x);
 }
 int main()
 {
-    int a[] = {1,2,3,4};
-    int res[4];
-    for(int i=0;i<3;i++)
-        cout<<a[i]<<" ";
-    transform(a,a+4,res,increment);
-    cout<<"\nAfter Increment"<<endl;
-    for(int i=0;i<3;i++)
-        cout<<res[i]<<" ";
-    return(0);
+    int a[] = {1, 2, 3, 4, 5};
+    int r[5];
+    transform(a, a + 5, r, increment);
+    for (int i = 0; i < 5; i++)
+        cout << r[i] << " ";
+    return (0);
 }
