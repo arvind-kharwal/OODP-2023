a03c867c55e72ff63e20943db2c5c9859ad77b08
Arvind Kharwal <karvind629@gmail.com>
2023-05-02 10:41:26 +0530

    2 May 2023

--- Unit 5/10. Function Object/6.fo.cpp
+++ Unit 5/10. Function Object/6.fo.cpp
@@ -6,7 +6,7 @@ int main()
 {
     vector<int> v1 = {1, 2, 3, 4, 5};
     vector<int> v2 = {10, 20, 30, 40, 50};
-    vector<int> v;
+    vector<int> v(5);
     transform(v1.begin(), v1.end(), v2.begin(), v1.begin(), plus<int>());
     vector<int>::iterator i;
     for (i = v1.begin(); i != v1.end(); i++)
